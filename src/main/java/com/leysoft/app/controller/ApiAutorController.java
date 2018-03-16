a52609efbf406adda01aa23e5c53c9138b8c4a4d
dabliuw22 <wleylop22@gmail.com>
2018-03-15 21:57:46 -0500

    Modificando ApiAutorController

--- src/main/java/com/leysoft/app/controller/ApiAutorController.java
+++ src/main/java/com/leysoft/app/controller/ApiAutorController.java
@@ -1,16 +1,11 @@
 package com.leysoft.app.controller;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
